c92c23c7d575901a7ae5860327d76725188643f5
ancooper <antonkudrin.perm@gmail.com>
2013-05-27 06:43:54 +0600

    стратегические порталы всегда видны

--- build/build.tamper.js
+++ build/build.tamper.js
@@ -13,61 +13,142 @@
 function wrapper() {
     // ensure plugin framework is there, even if iitc is not yet loaded
     if(typeof window.plugin !== 'function') window.plugin = function() {};
-    
+
+    Array.prototype.remove = function () {
+    	var what, a = arguments, L = a.length, ax;
+    	while (L && this.length) {
+    		what = a[--L];
+    		while ((ax = this.indexOf(what)) !== -1) {
+    			this.splice(ax, 1);
+    		}
+    	}
+    	return this;
+    };
+
     // PLUGIN START ////////////////////////////////////////////////////////
-    
-    // use own namespace for plugin
-    window.plugin.strategyManager = function() {};
-    
-    window.plugin.strategyManager.setup = function() {
-        //remove form array
-        Array.prototype.remove = function() {
-            var what, a = arguments, L = a.length, ax;
-            while (L && this.length) {
-                what = a[--L];
-                while ((ax = this.indexOf(what)) !== -1) {
-                    this.splice(ax, 1);
-                }
-            }
-            return this;
-        };
-        
-        //setup plugin
-        
-        //operation
-        window.operation = function () {};
-        window.operation.guid = 'abcdef01234566789';
-        window.operation.title = 'Perm eclipse (Пермское затмение)';
-        
-        window.operation.portals = [];
-        window.operation.tasks = [];
-        
-        window.operation.isStrategicPortal = function (guid) {
-            return window.operation.portals.indexOf(guid) != -1;    
-        }
-        
-        window.plugin.strategyManager.setupStrategyTab();
-        
-        window.plugin.strategyManager.drawnStrategy = new L.LayerGroup();
-        window.layerChooser.addOverlay(window.plugin.strategyManager.drawnStrategy, 'Strategy');
-        map.addLayer(window.plugin.strategyManager.drawnStrategy);
-        map.on('layeradd',function(obj) {
-            if(obj.layer === window.plugin.strategyManager.drawnStrategy)
-            {
-                obj.layer.eachLayer(function(marker) {
-                    if(marker._icon) window.setupTooltips($(marker._icon));
-                });
-            }
-        });
-        
-        window.addHook('portalDetailsUpdated', window.plugin.strategyManager.portalDetailsUpdated);
+
+    // Overrides IITC methods
+    window.portalRenderLimit.cleanUpOverLimitPortalLevel = function () {
+    	var currentMinLevel = window.getMinPortalLevel();
+    	for (var i = 0; i < currentMinLevel; i++) {
+    		portalsLayers[i].eachLayer(function (item) {
+    			var itemGuid = item.options.guid;
+    			// check if 'item' is a portal
+    			if (getTypeByGuid(itemGuid) != TYPE_PORTAL) return true;
+    			// Don’t remove if it is selected.
+    			if (itemGuid == window.selectedPortal) return true;
+    			// Don’t remove if it is strategic.
+    			if (window.operation.portals.indexOf(itemGuid) != -1) return true;
+    			portalsLayers[i].removeLayer(item);
+    		});
+    	}
     }
-    
-    window.plugin.strategyManager.drawData = function() {
-        window.plugin.strategyManager.drawnStrategy.clearLayers();
+    // End overrides
+
+    window.plugin.ism = function () { };
+    window.plugin.ism.operation = {};
+	/*
+	window.plugin.ism.addIsmControl = function() {
+	  var IsmControl = new L.Control.Draw({
+		draw: {
+		  rectangle: false,
+		  polygon: {
+			title: 'Add a polygon\n\n'
+			  + 'Click on the button, then click on the map to\n'
+			  + 'define the start of the polygon. Continue clicking\n'
+			  + 'to draw the line you want. Click the first or last\n'
+			  + 'point of the line (a small white rectangle) to\n'
+			  + 'finish. Double clicking also works.'
+		  },
+
+		  polyline: {
+			title: 'Add a (poly) line.\n\n'
+			  + 'Click on the button, then click on the map to\n'
+			  + 'define the start of the line. Continue clicking\n'
+			  + 'to draw the line you want. Click the <b>last</b>\n'
+			  + 'point of the line (a small white rectangle) to\n'
+			  + 'finish. Double clicking also works.'
+		  },
+
+		  circle: {
+			title: 'Add a circle.\n\n'
+			  + 'Click on the button, then click-AND-HOLD on the\n'
+			  + 'map where the circle’s center should be. Move\n'
+			  + 'the mouse to control the radius. Release the mouse\n'
+			  + 'to finish.'
+		  },
+
+		  marker: {
+			title: 'Add a marker.\n\n'
+			  + 'Click on the button, then click on the map where\n'
+			  + 'you want the marker to appear.'
+		  },
+
+		},
+
+		edit: {
+		  featureGroup: window.plugin.drawTools.drawnItems,
+
+		  edit: {
+			title: 'Edit drawn items'
+		  },
+
+		  remove: {
+			title: 'Delete drawn items'
+		  },
+
+		},
+
+	  });
+
+	  map.addControl(drawControl);
+	//  plugin.drawTools.addCustomButtons();
+	}
+	*/
+
+    window.plugin.ism.test1 = function () { alert('test1'); }
+    window.plugin.ism.test2 = function () { alert('test2'); }
+
+    window.plugin.ism.setup = function () {
+    	self = this;
+
+    	//setup plugin
+    	$('#toolbox')
+			.append('<a onclick="window.plugin.ism.test1()">Test#1</a>')
+			.append('<a onclick="window.plugin.ism.test2()">Test#2</a>');
+    	//window.map.on('click', window.plugin.ism.onMapClick
+
+    	//operation
+    	this.operation = {
+			guid: 'abcdef01234566789',
+			title: 'Perm eclipse (Пермское затмение)',
+    		portals: [],
+			links: [],
+    		tasks: []
+		};
+    	
+    	this.isStrategicPortal = function (guid) {
+    		return window.operation.portals.indexOf(guid) != -1;
+    	}
+
+    	this.setupStrategyTab();
+    	//this.setupStrategyLayer();
+
+    	window.addHook('portalDetailsUpdated', this.portalDetailsUpdated);
+    }
+
+    window.plugin.ism.setupStrategyLayer = function () {
+    	this.layer = new L.LayerGroup();
+    	window.layerChooser.addOverlay(this.layer, 'Strategy');
+    	window.map.addLayer(this.layer);
+    }
+
+
+    window.plugin.ism.drawData = function() {
+        this.layer.clearLayers();
         $('#portalstable').empty();
         $('#linkstable').empty();
-        $.each(window.operation.portals, function(i, guid) {
+        $.each(this.operation.portals, function(i, guid) {
             var d = window.portals[guid].options.details
             var coord = [d.locationE6.latE6/1E6, d.locationE6.lngE6/1E6];
             var marker = new L.CircleMarker(coord, {
@@ -79,7 +160,7 @@ function wrapper() {
                 fillOpacity: 0.25,
                 clickable: true
             })
-            marker.addTo(window.plugin.strategyManager.drawnStrategy);
+            marker.addTo(window.plugin.ism.drawnStrategy);
             marker.on('click', function () { console.log('click to strat portal'); });
             var portal = $('<tr><td style="width: 80%">'+d.portalV2.descriptiveText.TITLE+'</td><td style="width: 20%"><a>X</a></td></tr>');
             $('#portalstable').append(portal);
@@ -89,44 +170,43 @@ function wrapper() {
         
     }    
     
-    window.plugin.strategyManager.renderStrategicPortalToggleButton = function () {
-        var isStrategic = window.operation.isStrategicPortal(window.selectedPortal) ? 'YES' : 'NO';
+    window.plugin.ism.renderStrategicPortalToggleButton = function () {
+        var isStrategic = this.isStrategicPortal(window.selectedPortal) ? 'YES' : 'NO';
         var button = $('#strategicToggleButton');
         if(button.length == 0){
-            button = $('<a id="strategicToggleButton" onclick="window.plugin.strategyManager.toggleStrategicPortal()" title="Toggle strategic portal"></a>');
+            button = $('<a id="strategicToggleButton" onclick="window.plugin.ism.toggleStrategicPortal()" title="Toggle strategic portal"></a>');
             $('#portaldetails .linkdetails').append($('<aside>').append(button));
         }
         button.text('Strategic portal: ' + isStrategic);
     }
     
-    window.plugin.strategyManager.portalDetailsUpdated = function (data) {
-        window.plugin.strategyManager.renderStrategicPortalToggleButton();
+    window.plugin.ism.portalDetailsUpdated = function (data) {
+        this.renderStrategicPortalToggleButton();
         return true;
     }
     
-    window.plugin.strategyManager.toggleStrategicPortal = function () {
+    window.plugin.ism.toggleStrategicPortal = function () {
         var guid = window.selectedPortal;
-        if(window.operation.isStrategicPortal(guid)) {
-            window.plugin.strategyManager.removeStrategicPortal(guid);
+        if(this.isStrategicPortal(guid)) {
+            this.removeStrategicPortal(guid);
         } else {
-            window.plugin.strategyManager.addStrategicPortal(guid);
+            this.addStrategicPortal(guid);
         }
-        plugin.strategyManager.drawData();
-        
+        //this.drawData();
     }
     
-    window.plugin.strategyManager.addStrategicPortal = function (guid) {
-        window.operation.portals.push(guid);
-        window.plugin.strategyManager.renderStrategicPortalToggleButton(guid);
+    window.plugin.ism.addStrategicPortal = function (guid) {
+        this.operation.portals.push(guid);
+        this.renderStrategicPortalToggleButton(guid);
     }
     
-    window.plugin.strategyManager.removeStrategicPortal = function (guid) {
-        window.operation.portals.remove(guid);
-        window.plugin.strategyManager.renderStrategicPortalToggleButton(guid);
+    window.plugin.ism.removeStrategicPortal = function (guid) {
+        this.operation.portals.remove(guid);
+        this.renderStrategicPortalToggleButton(guid);
     }
     
-    window.plugin.strategyManager.setupStrategyTab = function () {
-        $('#chatcontrols').append($('<a id="strategy" onclick="window.plugin.strategyManager.activateStrategyTab();">strategy</a>'));
+    window.plugin.ism.setupStrategyTab = function () {
+        $('#chatcontrols').append($('<a id="strategy" onclick="window.plugin.ism.activateStrategyTab();">strategy</a>'));
         $('#chat').append($('<div id="strategytab"></div>'));
         
         $('#strategytab').html
@@ -149,7 +229,7 @@ function wrapper() {
         
     }
     
-    window.plugin.strategyManager.activateStrategyTab = function () {
+    window.plugin.ism.activateStrategyTab = function () {
         $('#chat > div').hide();
         $('#chatcontrols a').removeClass('active');
         $('#strategytab').show();
@@ -157,7 +237,7 @@ function wrapper() {
     }
     
     var setup =  function() {
-        window.plugin.strategyManager.setup();
+        window.plugin.ism.setup();
     }
     
     // PLUGIN END //////////////////////////////////////////////////////////
